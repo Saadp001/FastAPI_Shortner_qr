06f4f910145ca9250e160e0b5ff5cc3d220f0bed
Saadp001 <psaad2988@gmail.com>
2025-12-01 16:28:55 +0530

    Add CORS middleware for handling cross-origin requests
    Added CORS middleware to allow cross-origin requests.

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.database import Base, engine
 from src.routers import shortener, redirect
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 # Create tables 
 Base.metadata.create_all(bind=engine)
@@ -12,3 +13,12 @@ app.include_router(shortener.router)
 app.include_router(redirect.router)
 
 app.mount("/qr", StaticFiles(directory="qr_codes"), name="qr")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all domains for now
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
