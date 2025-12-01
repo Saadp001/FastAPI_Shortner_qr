46a462a0685d9dcd8baf5e689f45ba03070b3356
Saadp001 <psaad2988@gmail.com>
2025-12-01 15:29:27 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,19 +1,20 @@
-# app/database.py
+# src/database.py
 
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
+# Pull DB URL from environment variable (Render)
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    pool_pre_ping=True,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
