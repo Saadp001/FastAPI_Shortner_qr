9092304400b2c00ae0d966d6d7bcae5a5943cf7c
Saadp001 <psaad2988@gmail.com>
2025-12-01 22:42:19 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,7 +12,13 @@ app = FastAPI()
 app.include_router(shortener.router)
 app.include_router(redirect.router)
 
-app.mount("/qr", StaticFiles(directory="qr_codes"), name="qr")
+# app.mount("/qr", StaticFiles(directory="qr_codes"), name="qr")
+from fastapi.responses import FileResponse
+
+@app.get("/qr/{file_name}")
+def get_qr(file_name: str):
+    file_path = f"qr_codes/{file_name}"
+    return FileResponse(path=file_path, media_type="image/png")
 
 
 app.add_middleware(
